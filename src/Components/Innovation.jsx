490816a17e5c35f068dae308b87aae14e7880689
AhmedSaeed216 <ahmed.saeed6420@gmail.com>
2024-09-12 15:54:35 +0500

    add the innovation file in the feature2 branch

---
+++ src/Components/Innovation.jsx
@@ -0,0 +1,35 @@
+import React,{useState,useEffect} from 'react'
+import NewsDetail from './NewsDetail'
+import InnoImg from '../assets/innovation.jpg'
+
+const Innovation = () => {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+  const url=`https://newsapi.org/v2/everything?q=innovation&apiKey=ca9ec3f42e9f498eba199024ba4934c4`  
+  fetch(url)
+    .then(response=>response.json())
+    .then(data=>setArticles(data.articles))
+  }, [])
+  
+  return (
+    <>
+    <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4 '>
+      {articles.map((data,index)=>(
+            data.title?(
+          <NewsDetail 
+          key={index}
+          src={data.urlToimage}
+          url={data.url}
+          description={data.description}
+          title={data.title}
+          altImg={InnoImg}
+          />
+        ):( <p className='flex justify-center items-center font-bold text-3xl' key={index}>No data found </p>)
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default Innovation
